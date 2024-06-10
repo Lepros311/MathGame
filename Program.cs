19273907a0f2eb68c2897e73d868ee7589ad3f3d
Lepros311 <andy.strickland@gmail.com>
2024-06-10 11:05:49 -0400

    Add stretch goal features except for timer

--- Program.cs
+++ Program.cs
@@ -1,32 +1,111 @@
-﻿int sum;
+﻿using System;
+using System.Diagnostics;
+
+int sum;
 int difference;
 int product;
 int quotient;
 
-string? userChoice = "0";
-string[] options = { "1", "2", "3", "4", "5", "6" };
+string? userGameChoice = "0";
+string[] gameOptions = { "1", "2", "3", "4", "5", "6", "7" };
 string gameHistory = "";
 int points = 0;
 
-while (userChoice != "6")
+
+// Try to implement levels of difficulty.
+
+// Add a timer to track how long the user takes to finish the game.
+
+// Add a function that let's the user pick the number of questions.
+
+// Create a 'Random Game' option where the players will be presented with questions from random operations
+
+string? userDifficultyChoice;
+string[] difficultyOptions = { "1", "2", "3" };
+
+
+Console.WriteLine("\nWelcome to the Math Game!");
+
+do
+{
+    Console.WriteLine("\n1) Low 2) Medium 3) High");
+    Console.WriteLine("Select difficulty level:");
+    userDifficultyChoice = Console.ReadLine();
+    if (!difficultyOptions.Contains(userDifficultyChoice))
+    {
+        Console.WriteLine("That is not a valid option. You must enter 1, 2, or 3.");
+    }
+} while (!difficultyOptions.Contains(userDifficultyChoice));
+
+string? userMaxQuestionsStr;
+int userMaxQuestionsInt;
+bool validUserMaxQuestions = false;
+
+do
+{
+    Console.WriteLine("Maximum number of questions:");
+    userMaxQuestionsStr = Console.ReadLine();
+    if (int.TryParse(userMaxQuestionsStr, out userMaxQuestionsInt))
+    {
+        if (userMaxQuestionsInt < 1 || userMaxQuestionsInt > 100)
+        {
+            Console.WriteLine("That is not a valid option. You must enter a number between 1 and 100.");
+        }
+        else
+        {
+            validUserMaxQuestions = true;
+        }
+    }
+} while (validUserMaxQuestions == false);
+
+int questionsAsked = 0;
+
+while (userGameChoice != "7" && questionsAsked < userMaxQuestionsInt)
 {
     Random random = new Random();
-    int num1 = random.Next(101);
-    int num2 = random.Next(1, 11);
+    int num1 = 0;
+    int num2 = 0;
+
+    if (userDifficultyChoice == "1")
+    {
+        num1 = random.Next(11);
+        num2 = random.Next(1, 11);
+    }
+    else if (userDifficultyChoice == "2")
+    {
+        num1 = random.Next(101);
+        num2 = random.Next(1, 101);
+    }
+    else if (userDifficultyChoice == "3")
+    {
+        num1 = random.Next(1001);
+        num2 = random.Next(1, 1001);
+    }
 
     do
     {
         Console.WriteLine("\n-------------------------------------");
-        Console.WriteLine("1) Addition 2) Subtraction 3) Multiplication 4) Division 5) View Game History 6) End game");
-        Console.WriteLine("Make a selection. Enter 1, 2, 3, 4, 5, or 6:");
-        userChoice = Console.ReadLine();
-        if (!options.Contains(userChoice))
+        Console.WriteLine("1) Addition 2) Subtraction 3) Multiplication 4) Division 5) Random! (+, -, *, or /) 6) View Game History 7) End game");
+        Console.WriteLine("Make a selection. Enter 1, 2, 3, 4, 5, 6, or 7:");
+        userGameChoice = Console.ReadLine();
+        if (!gameOptions.Contains(userGameChoice))
         {
-            Console.WriteLine("That is not a valid option. You must enter 1, 2, 3, 4, 5, or 6.");
+            Console.WriteLine("That is not a valid option. You must enter 1, 2, 3, 4, 5, 6, or 7.");
         }
-    } while (!options.Contains(userChoice));
+    } while (!gameOptions.Contains(userGameChoice));
 
-    switch (userChoice)
+    if (userGameChoice == "5")
+    {
+        userGameChoice = random.Next(1, 5).ToString();
+    }
+
+    Stopwatch timer = new Stopwatch();
+    timer.Start();
+    Console.WriteLine("Timer started...");
+
+    Console.WriteLine($"Question #{questionsAsked + 1} out of {userMaxQuestionsInt}:");
+
+    switch (userGameChoice)
     {
         case "1":
             Console.WriteLine($"What is {num1} + {num2}?");
@@ -43,48 +122,64 @@ while (userChoice != "6")
         case "4":
             while (num1 % num2 != 0)
             {
-                num1 = random.Next(101);
-                num2 = random.Next(1, 11);
+                if (userDifficultyChoice == "1")
+                {
+                    num1 = random.Next(11);
+                    num2 = random.Next(1, 11);
+                }
+                else if (userDifficultyChoice == "2")
+                {
+                    num1 = random.Next(101);
+                    num2 = random.Next(1, 101);
+                }
+                else if (userDifficultyChoice == "3")
+                {
+                    num1 = random.Next(1001);
+                    num2 = random.Next(1, 1001);
+                }
             }
             Console.WriteLine($"What is {num1} / {num2}?");
             break;
 
-        case "5":
+        case "6":
             Console.WriteLine("\nYour Game History:");
             Console.WriteLine(gameHistory);
+            Console.WriteLine($"Timer: {timer.Elapsed}");
             Console.WriteLine($"Total Points: {points}");
             break;
 
-        case "6":
+        case "7":
+            timer.Stop();
             Console.WriteLine("\nPost-Game Report:");
             Console.WriteLine(gameHistory);
             Console.WriteLine($"Total Points: {points}");
+            Console.WriteLine($"Timer: {timer.Elapsed}");
             Console.WriteLine("\nThanks for playing! Goodbye!");
             break;
     }
 
     string? userAnswerStr;
     int userAnswerInt = 0;
-    bool validEntry = false;
+    bool validUserAnswer = false;
 
-    if (userChoice != "5" && userChoice != "6")
+    if (userGameChoice != "6" && userGameChoice != "7")
     {
         do
         {
             userAnswerStr = Console.ReadLine();
             if (userAnswerStr != null)
             {
-                validEntry = int.TryParse(userAnswerStr, out userAnswerInt);
-                if (!validEntry)
+                validUserAnswer = int.TryParse(userAnswerStr, out userAnswerInt);
+                if (!validUserAnswer)
                 {
                     Console.WriteLine("Invalid entry. Enter an integer.");
                 }
             }
-        } while (validEntry == false);
+        } while (validUserAnswer == false);
     }
 
 
-    if (userChoice == "1")
+    if (userGameChoice == "1")
     {
         sum = num1 + num2;
         if (sum == userAnswerInt)
@@ -99,7 +194,7 @@ while (userChoice != "6")
             gameHistory += $"{num1} + {num2} = {sum}...Answered incorrectly\n";
         }
     }
-    else if (userChoice == "2")
+    else if (userGameChoice == "2")
     {
         difference = num1 - num2;
         if (difference == userAnswerInt)
@@ -114,7 +209,7 @@ while (userChoice != "6")
             gameHistory += $"{num1} - {num2} = {difference}...Answered incorrectly\n";
         }
     }
-    else if (userChoice == "3")
+    else if (userGameChoice == "3")
     {
         product = num1 * num2;
         if (product == userAnswerInt)
@@ -129,7 +224,7 @@ while (userChoice != "6")
             gameHistory += $"{num1} * {num2} = {product}...Answered incorrectly\n";
         }
     }
-    else if (userChoice == "4")
+    else if (userGameChoice == "4")
     {
         quotient = num1 / num2;
         if (quotient == userAnswerInt)
@@ -143,6 +238,17 @@ while (userChoice != "6")
             Console.WriteLine($"Incorrect! The answer is {quotient}.");
             gameHistory += $"{num1} / {num2} = {quotient}...Answered incorrectly\n";
         }
+    }
+    questionsAsked++;
+
+    if (questionsAsked == userMaxQuestionsInt)
+    {
+        timer.Stop();
+        Console.WriteLine("\nPost-Game Report:");
+        Console.WriteLine(gameHistory);
+        Console.WriteLine($"Total Points: {points}");
+        Console.WriteLine($"Timer: {timer.Elapsed}");
+        Console.WriteLine("\nThanks for playing! Goodbye!");
     }
 }
 
